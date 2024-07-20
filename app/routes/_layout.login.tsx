51ecb5e29f7883ee1f45288ad2fa511c4e15e2f9
Murthy Udupa <ugmurthy@gmail.com>
2024-07-20 13:03:24 +0530

    login form is responsive now

--- app/routes/_layout.login.tsx
+++ app/routes/_layout.login.tsx
@@ -76,10 +76,10 @@ if (data?.success) {
       <Google gid={google_client_id}></Google>
       <div className="divider px-40"> or </div>
       <Form  method="post" action="/login">
-        <div className="max-w-md mx-auto p-6 lg:p-8 text-gray-800 space-y-4 ">
-        <h1 className="text-center text-3xl">Login  </h1>
+        <div className="max-w-sm md:max-w-md mx-auto p-6 lg:p-8 text-gray-800 space-y-4 ">
+        <h1 className="text-center text-3xl">Login</h1>
       <div>
-      <label className="input input-bordered input-md flex items-center gap-2">
+      <label className="input input-bordered input-sm md:input-md flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
         <input name="email" type="text"  placeholder="Email" className="w-full"/>
         
@@ -87,7 +87,7 @@ if (data?.success) {
       {data?.emailError && <div className="font-thin text-red-700 ">{data.emailError}</div>}
       </div>
       <div>
-      <label className="input input-bordered flex items-center gap-2">
+      <label className="input input-bordered input-sm md:input-md flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
         <input name="password" type="password" className="w-full"  />
         
@@ -97,12 +97,12 @@ if (data?.success) {
         <div className="form-control">
           <label className="label cursor-pointer">
             <span className="label-text">Remember me</span> 
-            <input name="rememberMe" type="checkbox" defaultChecked className="checkbox" />
+            <input name="rememberMe" type="checkbox" defaultChecked className="checkbox checkbox-sm md:checkboc-md" />
           </label>
         </div>
         <div className="flex flex-row justify-between  ">
-        <button className="btn btn-neutral" type="submit">Login</button>
-        <Link to="/signup" className="underline cursor-pointer place-content-center">Sign Up</Link>
+        <button className="btn btn-neutral btn-sm md:btn-md" type="submit">Login</button>
+        <Link to="/signup" className="text-sm md:text-xl underline cursor-pointer place-content-center">Sign Up</Link>
         </div>
         </div>
       </Form>
